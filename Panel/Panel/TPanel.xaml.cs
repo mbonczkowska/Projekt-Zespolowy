c4f23b4ecbe108804e838cc8dae639d78a8a99e5
mbonczkowska <mbonczowska@sigma.ug.edu.pl>
2014-11-12 23:49:37 +0100

    Panel gry + Landscape

--- Panel/Panel/TPanel.xaml.cs
+++ Panel/Panel/TPanel.xaml.cs
@@ -25,6 +25,33 @@ namespace Panel
         public TPanel()
         {
             this.InitializeComponent();
+            Window.Current.SizeChanged +=Current_SizeChanged; // kiedy zmieni sie orientation
+        }
+
+        private void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+          
+            var b = Window.Current.Bounds;
+            if (b.Height > b.Width)
+            {
+                // Portrait
+                food1.Margin = new Thickness(0,0,0,0);
+                play2.Margin = new Thickness(133, 0, 0, 0);
+                stats3.Margin = new Thickness(266, 0, 0, 0);
+                micr4.Margin = new Thickness(0, 495, 0, 0);
+                med5.Margin = new Thickness(133, 495, 0, 0);
+                sleep6.Margin = new Thickness(266, 495, 0, 0);
+            }
+            else
+            {
+                // Landscape
+                food1.Margin = new Thickness(0, 0, 0, 0);
+                play2.Margin = new Thickness(0, 132, 0, 0);
+                stats3.Margin = new Thickness(0, 266, 0, 0);
+                micr4.Margin = new Thickness(471, 0, 0, 0);
+                med5.Margin = new Thickness(471, 132, 0, 0);
+                sleep6.Margin = new Thickness(471, 266, 0, 0);
+            }
         }
 
         /// <summary>
